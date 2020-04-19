e80ac9e7e155f212615ea0521098b09bbf36aa37
Madison London <47748676+mslondon99@users.noreply.github.com>
2020-04-19 12:29:00 -0400

    Started Edge and Vertex classes, adding a constructor to both

--- Edge.java
+++ Edge.java
@@ -1,1 +1,14 @@
- 
+public class Edge
+{
+    Vertex v1; // represents one of the vertices at the end of the edge
+    Vertex v2; // represents one of the vertices at the end of the edge
+    boolean hovered; // represents whether or not the user's cursor is hovering of the edge
+    double weight; // the weight associated with the edge
+    
+    public Edge(Vertex x, Vertex y) { // constructor when given two vertices
+        v1 = x;
+        v2 = y;
+        hovered = false;
+        weight = 1.0; // default edge weight is 1
+    }
+} 
--- Vertex.java
+++ Vertex.java
@@ -1,1 +1,12 @@
+import java.awt.Point;
 
+public class Vertex
+{
+    Point p;
+    boolean hovered;
+    
+    public Vertex(Point x) {
+        p = x;
+        highlighted = false;
+    }
+}
