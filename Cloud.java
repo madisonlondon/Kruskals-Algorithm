df6bc8b9a3ff286da0320ae5424824af28ec62c6
Berke Nuri <47748547+berkenuri@users.noreply.github.com>
2020-04-20 22:32:50 +0300

    Cloud class

---
+++ Cloud.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Cloud
+{
+    ArrayList vertices = new ArrayList();
+    
+    public Cloud() {
+    }
+    
+    public Cloud(Vertex v) {
+        vertices.add(v);
+    }
+    
+    public static void merge(Cloud c1, Cloud c2) {
+    
+    		for (int i = 0; i < c2.vertices.size(); i++) {
+    			
+    			Vertex v = (Vertex) c2.vertices.get(i);
+    			c1.vertices.add(v);
+        }
+    }
+}
